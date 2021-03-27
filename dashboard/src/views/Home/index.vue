e9b1ce32e101e2bf4255cac7090d81105180d94e
Alexandre <alexandre_alt@outlook.com>
2021-03-26 21:09:44 -0300

    created dashboard initial screen

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1 class="font-black text-brand-main">Hello Home :;)</h1>
+  <custom-header
+    @create-account="handleAccountCreate()"
+    @login="handleLogin()"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+  </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
 
+export default {
+  components: { CustomHeader, Contact },
+
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
